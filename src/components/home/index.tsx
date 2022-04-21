fbafb1a5029cd1b3861492aa4b9cd59e2ab9062d
Heloiza Mendes <helocursos@gmail.com>
2022-04-21 18:03:22 -0300

    grid item creation

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,15 +1,16 @@
 
 import { useState } from 'react';
 import styles from '../../App.module.css'
-
+import { levels, calculaImc } from '../../helpers/Imc';
 import poweredImage from '../../assests/powered.png';
+import { GridItem } from '../GridItem/index'
 export const Home = () => {
+   
     const [heightField, setHeightField] = useState<number>(0);
     const [weightField, setWeightField] = useState<number>(0);
+
     const handleCalculateButton = () => {
         if (heightField && weightField) {
-
-
         } else {
             alert("Digite todos os campos");
         }
@@ -33,6 +34,7 @@ export const Home = () => {
                         type="number"
                         placeholder='Digite a sua altura. Ex: 1.5 (em metros)'
                         value={heightField > 0 ? heightField : ''}
+                                             
                         onChange={e => setHeightField(parseFloat(e.target.value))}
                     />
 
@@ -42,14 +44,21 @@ export const Home = () => {
                         value={weightField > 0 ? weightField : ''}
                         onChange={e => setWeightField(parseFloat(e.target.value))}
                     />
-
                     <button onClick={handleCalculateButton}>Calcular</button>
+                </div>
 
-                </div>
-                <div className={styles.rightSide}>
-                </div>
+                       <div className={styles.rightSide}>
+                       <div className={styles.grid}>
+                        {levels.map((item, key)=>(
+                    <GridItem key={key} item={item}/>
+                   
+                   ))}
+                       
+                       </div>
+                  </div>
             </div>
         </div>
+
     );
 
 }
